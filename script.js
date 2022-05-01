a16ec4de829cc84212c6907ee4b04213fd5e2732
Nejc Furh <101887698+NF-7@users.noreply.github.com>
2022-05-01 22:23:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-/* Examples of DOM Manipulation
-
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
-
-document.querySelector('.number').textContent = 13;
-
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-
-console.log(document.querySelector('.guess').value);
-
-*/
-
 // STEP BY STEP CODE
 
 // CODING CHALLENGE - real solution bellow reseting the whole page
